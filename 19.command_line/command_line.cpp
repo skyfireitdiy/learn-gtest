99a6b0fdce9b5ac73cc9343e25c85a3517584979
skyfire <skyfireitdiy@hotmail.com>
2024-02-05 00:09:45 +0800

    增加命令行用例

---
+++ 19.command_line/command_line.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+
+TEST(FooTest, Foo1)
+{
+    EXPECT_EQ(1, 1);
+    EXPECT_EQ(2, 2);
+}
+
+TEST(FooTest, Foo2)
+{
+    EXPECT_EQ(1, 1);
+    EXPECT_EQ(2, 2);
+}
+
+TEST(BarTest, Bar1)
+{
+    EXPECT_EQ(1, 1);
+    EXPECT_EQ(2, 2);
+}
+
+TEST(BarTest, Bar2)
+{
+    EXPECT_EQ(1, 1);
+    EXPECT_EQ(2, 2);
+}
