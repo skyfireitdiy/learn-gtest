aea4e51309a573374534d9aa88fa97b49f224539
skyfire <skyfireitdiy@hotmail.com>
2024-02-04 20:54:12 +0800

    增加expect and assert用例

---
+++ 16.expect_and_assert/expect_and_assert.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+
+TEST(ExpectAndAssertTest, Expect1)
+{
+    EXPECT_EQ(1, 2);
+    EXPECT_EQ(3, 4);
+}
+
+TEST(ExpectAndAssertTest, Assert1)
+{
+    ASSERT_EQ(1, 2);
+    ASSERT_EQ(3, 4);
+}
+
+void sub_routine_expect()
+{
+    EXPECT_EQ(1, 2);
+    EXPECT_EQ(3, 4);
+}
+
+void sub_routine_assert()
+{
+    ASSERT_EQ(1, 2);
+    ASSERT_EQ(3, 4);
+}
+
+TEST(ExpectAndAssertTest, Expect2)
+{
+    sub_routine_expect();
+}
+
+TEST(ExpectAndAssertTest, Assert2)
+{
+    sub_routine_assert();
+}
