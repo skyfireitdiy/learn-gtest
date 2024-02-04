c2a6c0aac717bf2458f0cb599d259ba0fe7dd17c
skyfire <skyfireitdiy@hotmail.com>
2024-02-04 21:05:32 +0800

    补充场景

--- 16.expect_and_assert/expect_and_assert.cpp
+++ 16.expect_and_assert/expect_and_assert.cpp
@@ -20,16 +20,18 @@ void sub_routine_expect()
 
 void sub_routine_assert()
 {
-    ASSERT_EQ(1, 2);
+    ASSERT_EQ(0, 2);
     ASSERT_EQ(3, 4);
 }
 
 TEST(ExpectAndAssertTest, Expect2)
 {
     sub_routine_expect();
+    EXPECT_EQ(5, 6);
 }
 
 TEST(ExpectAndAssertTest, Assert2)
 {
     sub_routine_assert();
+    ASSERT_EQ(5, 6);
 }
