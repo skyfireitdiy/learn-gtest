6195c30811beccc09e88467f1e291ce96a1324d6
skyfire <skyfireitdiy@hotmail.com>
2024-02-17 21:10:49 +0800

    补充参数相关测试

--- 21.call_with_param/call_with_param.cpp
+++ 21.call_with_param/call_with_param.cpp
@@ -1,4 +1,5 @@
 #include "gmock/gmock.h"
+#include "gtest/gtest.h"
 #include <gmock/gmock.h>
 
 class Calc
@@ -20,10 +21,26 @@ void UseCalc(Calc &c, int a, int b)
     c.Do(a, b);
 }
 
-TEST(Calc, MockCalcTest)
+TEST(Calc, MockCalcTestCase1)
 {
     MockCalc calc;
-    EXPECT_CALL(calc, Do(5, _));
+    EXPECT_CALL(calc, Do(5, _)); // 第一个参数必须是5，第二个随意
 
     UseCalc(calc, 5, 10);
+}
+
+TEST(Calc, MockCalcTestCase2)
+{
+    MockCalc calc;
+    EXPECT_CALL(calc, Do(testing::Gt(5), testing::Lt(10))); // 第一个参数必须大于5，第二个参数必须小于10
+
+    UseCalc(calc, 6, 9);
+}
+
+TEST(Calc, MockCalcTestCase3)
+{
+    MockCalc calc;
+    EXPECT_CALL(calc, Do);
+
+    UseCalc(calc, 0, 0);
 }
