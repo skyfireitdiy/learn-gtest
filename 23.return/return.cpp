86191252d96db136174ca93803fa9d5dfe7b36ba
skyfire <skyfireitdiy@hotmail.com>
2024-02-17 21:33:31 +0800

    补充返回用例

--- 23.return/return.cpp
+++ 23.return/return.cpp
@@ -24,7 +24,8 @@ using testing::Return;
 TEST(Calc, MockCalcTestCase1)
 {
     MockCalc calc;
-    EXPECT_CALL(calc, Do).WillOnce(Return(10));
+    EXPECT_CALL(calc, Do)
+        .WillOnce(Return(10));
 
     EXPECT_EQ(UseCalc(calc, 5, 10), 10);
 }
@@ -33,9 +34,42 @@ TEST(Calc, MockCalcTestCase2)
 {
     MockCalc calc;
 
-    EXPECT_CALL(calc, Do).Times(3).WillOnce(Return(10)).WillRepeatedly(Return(20));
+    EXPECT_CALL(calc, Do)
+        .Times(3)
+        .WillOnce(Return(10))
+        .WillRepeatedly(Return(20));
 
     EXPECT_EQ(UseCalc(calc, 5, 10), 10);
     EXPECT_EQ(UseCalc(calc, 5, 10), 20);
     EXPECT_EQ(UseCalc(calc, 5, 10), 20);
+}
+
+TEST(Calc, MockCalcTestCase3)
+{
+    MockCalc calc;
+    int n = 10;
+
+    EXPECT_CALL(calc, Do)
+        .Times(3)
+        .WillRepeatedly(Return(n++));
+
+    EXPECT_EQ(UseCalc(calc, 5, 10), 10);
+    EXPECT_EQ(UseCalc(calc, 5, 10), 10);
+    EXPECT_EQ(UseCalc(calc, 5, 10), 10);
+}
+
+TEST(Calc, MockCalcTestCase4)
+{
+    MockCalc calc;
+    int n = 10;
+
+    EXPECT_CALL(calc, Do)
+        .Times(3)
+        .WillOnce(Return(n++))
+        .WillOnce(Return(n++))
+        .WillOnce(Return(n++));
+
+    EXPECT_EQ(UseCalc(calc, 5, 10), 10);
+    EXPECT_EQ(UseCalc(calc, 5, 10), 11);
+    EXPECT_EQ(UseCalc(calc, 5, 10), 12);
 }
