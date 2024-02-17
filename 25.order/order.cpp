6f570e2cac29f326cf6368e13553a9ac90e47099
skyfire <skyfireitdiy@hotmail.com>
2024-02-17 21:47:11 +0800

    增加执行顺序用例

---
+++ 25.order/order.cpp
@@ -0,0 +1,49 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+class Calc
+{
+public:
+    virtual int Do(int a, int b) = 0;
+    virtual int Do1(int a, int b) = 0;
+};
+
+class MockCalc : public Calc
+{
+public:
+    MOCK_METHOD(int, Do, (int a, int b), (override));
+    MOCK_METHOD(int, Do1, (int a, int b), (override));
+};
+
+int UseCalc(Calc &c, int a, int b, bool d)
+{
+    return d ? c.Do(a, b) : c.Do1(a, b);
+}
+
+using testing::_;
+using testing::InSequence;
+using testing::Return;
+
+TEST(Calc, MockCalcTestCase1)
+{
+    MockCalc calc;
+
+    EXPECT_CALL(calc, Do1);
+    EXPECT_CALL(calc, Do);
+
+    UseCalc(calc, 5, 10, true);
+    UseCalc(calc, 5, 10, false);
+}
+
+TEST(Calc, MockCalcTestCase2)
+{
+    MockCalc calc;
+
+    InSequence seq;
+
+    EXPECT_CALL(calc, Do1);
+    EXPECT_CALL(calc, Do);
+
+    UseCalc(calc, 5, 10, false);
+    UseCalc(calc, 5, 10, true);
+}
