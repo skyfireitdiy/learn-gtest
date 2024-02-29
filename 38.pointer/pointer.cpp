39de1e9872615dd2cf77c2af02aa63f920557d3e
skyfire <skyfireitdiy@hotmail.com>
2024-02-29 10:24:44 +0000

    增加指针用例

---
+++ 38.pointer/pointer.cpp
@@ -0,0 +1,30 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include <gmock/gmock-matchers.h>
+
+class Calc 
+{
+    public:
+    virtual int calc(int *pa, int *pb) = 0;
+};
+
+class MockCalc : public Calc
+{
+    public:
+    MOCK_METHOD(int, calc, (int *pa, int *pb),(override));
+};
+
+using testing::Pointee;
+using testing::Gt;
+using testing::Lt;
+
+TEST(TestCalc, Case1)
+{
+    MockCalc c;
+    EXPECT_CALL(c, calc(Pointee(Gt(3)), Pointee(Lt(6))));
+
+    int a = 10;
+    int b = 5;
+    c.calc(&a, &b);
+}
+
